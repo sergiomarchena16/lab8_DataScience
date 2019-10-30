7d49ef4c5343a0c18a4369ba0b4f2909e43e90e7
Pepe <mar15163@uvg.edu.gt>
2019-10-30 13:29:10 -0600

    juntando datos pagina 2

--- lab8.R
+++ lab8.R
@@ -3,3 +3,15 @@
 #Catedrática: Lynette García 
 #Sergio Marchena - José Zuñiga - Pablo Viana 
 
+install.packages("tidyverse")
+
+library(tidyverse)
+
+tempDatos2 <- setwd("./datos2")
+tempDatos2 = list.files(pattern = "*.txt")
+datos2 <- lapply(tempDatos2, function(x) read.delim(x, header = FALSE, sep = "|"))
+
+
+dfDatos2 <- datos2 %>% reduce(full_join)
+
+View(df)
