58dd0bef95d3c04dfbbf058382cfa5b18193d701
Pepe <mar15163@uvg.edu.gt>
2019-10-30 13:40:56 -0600

    Col names datos2

--- lab8.R
+++ lab8.R
@@ -13,4 +13,7 @@ datos2 <- lapply(tempDatos2, function(x) read.delim(x, header = FALSE, sep = "|"
 
 
 dfDatos2 <- datos2 %>% reduce(full_join)
+dfDatos2 <- dfDatos2[!(dfDatos2$V1=="Pais de Proveniencia"),]
+dfDatos2$V18 <- NULL
+names(dfDatos2) <- c("Pais de Proveniencia","Aduana de Ingreso","Fecha de la Poliza","Partida Arancelaria","Modelo del Vehiculo","Marca","Linea","Centimetros Cubicos","Distintivo","Tipo de Vehiculo","Tipo de Importador","Tipo Combustible","Asientos","Puertas","Tonelaje","Valor CIF","Impuesto")
 
