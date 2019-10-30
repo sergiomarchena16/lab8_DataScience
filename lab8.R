ed91f86fb857fd2ddbdca9a605bdb2e587a50db4
Pepe <mar15163@uvg.edu.gt>
2019-10-30 14:01:23 -0600

    Barplot

--- lab8.R
+++ lab8.R
@@ -17,3 +17,6 @@ dfDatos2 <- dfDatos2[!(dfDatos2$V1=="Pais de Proveniencia"),]
 dfDatos2$V18 <- NULL
 names(dfDatos2) <- c("Pais de Proveniencia","Aduana de Ingreso","Fecha de la Poliza","Partida Arancelaria","Modelo del Vehiculo","Marca","Linea","Centimetros Cubicos","Distintivo","Tipo de Vehiculo","Tipo de Importador","Tipo Combustible","Asientos","Puertas","Tonelaje","Valor CIF","Impuesto")
 
+datos2011 <- dfDatos2[grepl("2014", dfDatos2$`Fecha de la Poliza`),]
+
+barplot(sort(table(datos2011$`Pais de Proveniencia`), decreasing = TRUE)[1:5], main = "2014 - Top 5 paises" )
